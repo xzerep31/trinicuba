29bcf40e14433238d3f8893e6cc2e13e7e259f98
xzerep31 <pvnavarrete31@gmail.com>
2025-01-18 13:59:35 -0500

    Listo para mostrar a Yoan

--- public/js/vendor/form.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('formSelector').addEventListener('change', function() {
-    const forms = document.querySelectorAll('.form-container');
-    forms.forEach(form => form.style.display = 'none'); // Hide all forms
-    const selectedForm = this.value;
-    if (selectedForm) {
-        document.getElementById(selectedForm).style.display = 'block'; // Show selected form
-    }
-});
